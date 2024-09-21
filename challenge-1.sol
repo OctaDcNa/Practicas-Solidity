f3b556c76ff6a316230369273685373ce3ce973c
OctaDcNa <okitavio890gmes@gmail.com>
2024-09-21 14:02:32 -0300

    Add files via upload

---
+++ challenge-1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract Ownable {
+    address public immutable owner;
+
+    event pago(address indexed from, uint256 amount);
+
+    constructor(address _ownerAddress) payable {
+        require(_ownerAddress != address(0), "La address debe ser valida");
+        owner = _ownerAddress;
+    }
+
+    receive() external payable {
+        emit pago(msg.sender, msg.value);
+    }
+
+    function balanceContract() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Solo el owner puede retirar los fondos.");
+        payable(owner).transfer(address(this).balance);
+    }
+}
